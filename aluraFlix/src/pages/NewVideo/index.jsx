e731c768d725696c36910dcc135c4dfd15d5002d
Leonardo Moreira Louzas <leonardomlouzas@gmail.com>
2024-07-10 16:33:21 -0300

    fix Carousel height

--- aluraFlix/src/pages/NewVideo/index.jsx
+++ aluraFlix/src/pages/NewVideo/index.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import styles from "./NewVideo.module.css";
+import { useNavigate } from "react-router-dom";
 
 export default function NewVideo() {
   const [title, setTitle] = useState("");
@@ -7,6 +8,8 @@ export default function NewVideo() {
   const [description, setDescription] = useState("");
   const [thumbnail, setThumbnail] = useState("");
   const [url, setUrl] = useState("");
+
+  const navigate = useNavigate();
 
   const toSubmit = async (event) => {
     event.preventDefault();
@@ -38,6 +41,8 @@ export default function NewVideo() {
     } catch (error) {
       console.error("Error adding video:", error);
     }
+
+    navigate("/");
   };
 
   return (
@@ -45,55 +50,66 @@ export default function NewVideo() {
       <h1>Novo Vídeo</h1>
       <p>Complete o formulário para criar um novo card de vídeo.</p>
       <form onSubmit={toSubmit} className={styles.form}>
-        <label htmlFor="title">Título</label>
-        <input
-          type="text"
-          value={title}
-          onChange={(event) => setTitle(event.target.value)}
-          placeholder="Digite o título do vídeo"
-          className={styles.input}
-          required
-        />
-        <label htmlFor="category">Categoria</label>
-        <select
-          id="category"
-          value={category}
-          onChange={(event) => setCategory(event.target.value)}
-          className={styles.input}
-          required
-        >
-          <option value="front-end">Front-End</option>
-          <option value="back-end">Back-End</option>
-          <option value="mobile">Mobile</option>
-        </select>
-        <label htmlFor="thumbnail">Imagem</label>
-        <input
-          type="text"
-          value={thumbnail}
-          onChange={(event) => setThumbnail(event.target.value)}
-          placeholder="Digite a URL da imagem"
-          className={styles.input}
-          required
-        />
-        <label htmlFor="url">URL</label>
-        <input
-          type="text"
-          value={url}
-          onChange={(event) => setUrl(event.target.value)}
-          placeholder="Digite a URL do vídeo"
-          className={styles.input}
-          required
-        />
-        <label htmlFor="description">Descrição</label>
-        <textarea
-          type="text"
-          value={description}
-          onChange={(event) => setDescription(event.target.value)}
-          placeholder="Digite a descrição do vídeo"
-          className={styles.input}
-          required
-        />
-        <button>Enviar</button>
+        <div className={styles.wrapper}>
+          <label htmlFor="title">Título</label>
+          <input
+            type="text"
+            value={title}
+            onChange={(event) => setTitle(event.target.value)}
+            placeholder="Digite o título do vídeo"
+            className={styles.input}
+            required
+          />
+        </div>
+        <div className={styles.wrapper}>
+          <label htmlFor="category">Categoria</label>
+          <select
+            id="category"
+            value={category}
+            onChange={(event) => setCategory(event.target.value)}
+            className={`${styles.input} ${styles.select}`}
+            required
+          >
+            <option value="front-end">Front-End</option>
+            <option value="back-end">Back-End</option>
+            <option value="mobile">Mobile</option>
+          </select>
+        </div>
+        <div className={styles.wrapper}>
+          <label htmlFor="thumbnail">Imagem</label>
+          <input
+            type="text"
+            value={thumbnail}
+            onChange={(event) => setThumbnail(event.target.value)}
+            placeholder="Digite a URL da imagem"
+            className={styles.input}
+            required
+          />
+        </div>
+        <div className={styles.wrapper}>
+          <label htmlFor="url">URL</label>
+          <input
+            type="text"
+            value={url}
+            onChange={(event) => setUrl(event.target.value)}
+            placeholder="Digite a URL do vídeo"
+            className={styles.input}
+            required
+          />
+        </div>
+        <div className={styles.wrapper}>
+          <label htmlFor="description">Descrição</label>
+          <br />
+          <textarea
+            type="text"
+            value={description}
+            onChange={(event) => setDescription(event.target.value)}
+            placeholder="Digite a descrição do vídeo"
+            className={styles.input}
+            required
+          />
+        </div>
+        <button className={styles.button}>Enviar</button>
       </form>
     </main>
   );
